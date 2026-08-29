3fca8a9a664aeb1c9973e5c30ecc50162f108655
Agostina Etchebarren <agostina.etchebarren@correo.ucu.edu.uy>
2026-08-29 20:36:00 -0300

    simulation first version

--- src/configdb.py
+++ src/configdb.py
@@ -1,5 +1,8 @@
 import os
 
+import pymysql
+from dbutils.pooled_db import PooledDB
+
 class DataBaseConfig:
     DEBUG = True
     MYSQL_HOST = os.getenv("MYSQL_HOST", "ADADB")
@@ -7,7 +10,23 @@ class DataBaseConfig:
     MYSQL_USER = os.getenv("MYSQL_USER", '')
     MYSQL_PW = os.getenv("MYSQL_PW", '1234')
 
-
 configuration = {
     "development": DataBaseConfig
 }
+
+credentials = configuration['development']
+
+POOL = PooledDB(
+    creator=pymysql,
+    maxconnections=6,
+    mincached=2,
+    host=credentials.MYSQL_HOST,
+    user=credentials.MYSQL_USER,
+    password=credentials.MYSQL_PW,
+    database=credentials.MYSQL_DB,
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+def get_connection():
+    return POOL.connection()
--- src/init.sql
+++ src/init.sql
@@ -17,7 +17,7 @@ CREATE TABLE login (
 
 CREATE TABLE sensor (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    estado BOOLEAN NOT NULL,
+    activo BOOLEAN NOT NULL,
     roto BOOLEAN DEFAULT FALSE,
     version DECIMAL(10, 2) NOT NULL,
     ultimo_repuesto DATETIME NOT NULL
@@ -30,3 +30,11 @@ CREATE TABLE sensor_logs (
     log_fecha DATETIME NOT NULL,
     FOREIGN KEY (sensor_id) REFERENCES sensor(id)
 )
+
+INSERT INTO sensor (activo, roto, version, ultimo_repuesto) VALUES
+(TRUE, FALSE, 1.11, "2026-08-2OT13:00:00"),
+(TRUE, FALSE, 1.11, "2026-08-2OT13:00:00"),
+(TRUE, FALSE, 1.11, "2026-08-2OT13:00:00"),
+(TRUE, FALSE, 1.11, "2026-08-2OT13:00:00"),
+(FALSE, FALSE, 1.12, "2026-08-25T13:00:00"),
+(FALSE, FALSE, 1.12, "2026-08-25T13:00:00");
